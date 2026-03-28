b2d258d176af1ac187c591a522097f79c3e20dab
Mikhail <mihworkmail54@gmail.com>
2026-03-28 12:03:01 +0200

    added catalogue depth print limit condition

--- libraries/printing/PrintingCatalogue.py
+++ libraries/printing/PrintingCatalogue.py
@@ -6,22 +6,27 @@ class PrintingCatalogue(PrintingBasic):
         super().__init__()
 
     @staticmethod
-    def printCatalogueFromDictionary(dictionary: dict):
-        string = PrintingCatalogue.returnDictionaryIndecesAsString(dictionary)
+    def printCatalogueFromDictionary(dictionary: dict, depthLimit: int = 5):
+        string = PrintingCatalogue.returnDictionaryIndecesAsString(dictionary, '|', depthLimit)
+        print("Catalogue contents : ")
         print(string)
 
     @staticmethod
-    def returnDictionaryIndecesAsString(catalogue: dict|list, prefix: str='') -> str:
-        result = ''
+    def returnDictionaryIndecesAsString(catalogue: dict|list, prefix: str='', depthLimit: int = 5) -> str:
+        resultLine = ''
         pile = {}
+        coursor = depthLimit-1
+
         if isinstance(catalogue, dict):
             pile = catalogue.items()
-            
         if isinstance(catalogue, list):
             pile = enumerate(catalogue)
         
         for index, value in pile:
-            result += prefix+str(index)+" \n"
+            resultLine += prefix+"-"+str(index)+" \n"
             if isinstance(value, dict) or isinstance(value, list):
-                result += PrintingCatalogue.returnDictionaryIndecesAsString(value, prefix + " | ")
-        return result
+                if coursor == 0:
+                    resultLine += prefix+" | ... \n"
+                else:
+                    resultLine += PrintingCatalogue.returnDictionaryIndecesAsString(value, prefix + " |", coursor)
+        return resultLine
